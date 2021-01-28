fe825d6603adb64a6ff827a0c023425c22f953f4
HaniMohamed <hani.hussein94@gmail.com>
2021-01-29 00:03:15 +0200

    using postgresql

--- quiz_project/settings.py
+++ quiz_project/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'quiz_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "quize",
+        'USER': 'admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'db',
+        'PORT': 5432
     }
 }
 
