53981f7ed2ca0bb5379c7c11843a7b31bc740f9b
HaniMohamed <hani.hussein94@gmail.com>
2021-01-29 03:53:59 +0200

    get answers grouped by questions

---
+++ api/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+from quiz.models import Answer, Question, Choice
+import datetime
+
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Choice
+        fields = ('id', 'text',)
+
+
+class AnswerSerializer(serializers.ModelSerializer):
+    choice = ChoiceSerializer()
+
+    class Meta:
+        model = Answer
+        fields = ('created', 'id', 'choice')
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+
+    answers = AnswerSerializer(many=True, read_only=True)
+    answers_count = serializers.IntegerField(
+        source='answers.count',
+        read_only=True
+    )
+
+    class Meta:
+        model = Question
+        fields = ('text', 'answers_count', 'answers', )
--- api/views.py
+++ api/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework import generics
+
+from quiz.models import Answer, Question
+from .serializers import AnswerSerializer, QuestionSerializer
 
 # Create your views here.
+
+
+class AnswerAPIView(generics.ListAPIView):
+    queryset = Answer.objects.all()
+    serializer_class = AnswerSerializer
+
+
+class QuestionAPIView(generics.ListAPIView):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
